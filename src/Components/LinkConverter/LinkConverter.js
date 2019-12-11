6ba9c8bad3d6287c779a8ae0873491890c5011da
Martin Kruger <martin@martinkruger.com>
2019-12-11 18:36:17 +0200

    Adding Regex checks

--- src/Components/LinkConverter/LinkConverter.js
+++ src/Components/LinkConverter/LinkConverter.js
@@ -4,50 +4,101 @@ import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import { Paper, TextField, Button, Grid } from '@material-ui/core';
 
-function LinkConverter(props) {
-  return (
-    <div>
-      <Typography color="primary" variant="h4" component="h1" gutterBottom>
-        {props.appTitle}
-      </Typography>
-      <Typography color="primary" component="p">
-        Convert between a GitHub Pages and Repo link
-      </Typography>
-      <Container maxWidth="sm">
-        <Box my={4}>
-          <Paper>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  id="url-input"
-                  label="Enter URL"
-                  // placeholder="https://github.com/martink-rsa/github-link-converter"
-                  variant="outlined"
-                  fullWidth
-                />
+class LinkConverter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputURL: '',
+      convertedURL: '',
+      allowConvert: false
+    };
+  }
+
+  inputValidation = inputURL => {
+    const ghPagesRegex = /github\.io/;
+    const ghLinkRegex = /github\.com/;
+    const regexMatch =
+      inputURL.match(ghPagesRegex) || inputURL.match(ghLinkRegex);
+    if (regexMatch) {
+      this.setState({ allowConvert: true });
+    } else {
+      this.setState({ allowConvert: false });
+    }
+  };
+
+  handleChange = event => {
+    const inputURL = event.target.value;
+    this.setState(() => {
+      return { inputURL };
+    });
+    this.inputValidation(inputURL);
+  };
+
+  convertURL = () => {
+    const ghPagesRegex = /github\.io/;
+    const ghLinkRegex = /github\.com/;
+
+    const inputURL = this.state.inputURL;
+    console.log(inputURL);
+  };
+
+  render() {
+    return (
+      <div>
+        <Typography color="primary" variant="h4" component="h1" gutterBottom>
+          {this.props.appTitle}
+        </Typography>
+        <Typography color="primary" component="p">
+          Convert between a GitHub Pages and Repo link
+        </Typography>
+        <Container maxWidth="sm">
+          <Box my={4}>
+            <Paper>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    id="url-input"
+                    label="Enter URL"
+                    // placeholder="https://github.com/martink-rsa/github-link-converter"
+                    variant="outlined"
+                    fullWidth
+                    value={this.state.inputURL}
+                    onChange={this.handleChange}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    id="url-result"
+                    label="Result"
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  {this.state.allowConvert ? (
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={this.convertURL}
+                    >
+                      Convert URL
+                    </Button>
+                  ) : (
+                    <Button variant="contained" color="primary" disabled>
+                      Convert URL
+                    </Button>
+                  )}
+                  <Button variant="contained" color="secondary">
+                    Convert &amp; Go
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  id="url-result"
-                  label="Result"
-                  variant="outlined"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <Button variant="contained" color="primary">
-                  Convert URL
-                </Button>
-                <Button variant="contained" color="secondary">
-                  Convert &amp; Go
-                </Button>
-              </Grid>
-            </Grid>
-          </Paper>
-        </Box>
-      </Container>
-    </div>
-  );
+            </Paper>
+          </Box>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default LinkConverter;
