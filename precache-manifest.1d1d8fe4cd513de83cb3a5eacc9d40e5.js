f080d3359c9956648da18060cfdef71fc88f0d6d
Martin Kruger <martin@martinkruger.com>
2019-12-13 14:58:07 +0200

    Updates

---
+++ precache-manifest.1d1d8fe4cd513de83cb3a5eacc9d40e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cdff63a987632cd28335f6ca758d99",
+    "url": "/github-link-converter/index.html"
+  },
+  {
+    "revision": "64d39c08219954ac229f",
+    "url": "/github-link-converter/static/css/main.ffdbb45b.chunk.css"
+  },
+  {
+    "revision": "69fb75391a6821d5b5a4",
+    "url": "/github-link-converter/static/js/2.4f045301.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/github-link-converter/static/js/2.4f045301.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64d39c08219954ac229f",
+    "url": "/github-link-converter/static/js/main.fbb88cf0.chunk.js"
+  },
+  {
+    "revision": "1f36a25459af8ea2eb66",
+    "url": "/github-link-converter/static/js/runtime-main.5840c0ec.js"
+  }
+]);
